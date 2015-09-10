da04316cd47fabaf1922a10c6efcbfeaea3dfa55
Logan Graba <logangraba@gmail.com>
2015-09-10 05:28:52 -0600

    Project CRUD

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+	def index
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new project_params
+
+		if @project.save
+			redirect_to @project, notice: "You created a project, Logan!"
+		else
+			render 'new', notice: "Oh nooooo! Couldn't create project, Logan."
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Project updated, baby."
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:title, :content)
+	end
+
+	def find_project
+		@project = Project.find(params[:id])
+	end
+end

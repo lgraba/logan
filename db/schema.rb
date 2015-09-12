1aee03c3b8e176e6af1470778005a2f738b7ab83
Logan Graba <logangraba@gmail.com>
2015-09-12 03:04:12 -0600

    Added Project description

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910101439) do
+ActiveRecord::Schema.define(version: 20150912085153) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
 end

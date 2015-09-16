cf6d013ec89ce863bbfe8e6175c348b7a5bf3a73
Logan Graba <logangraba@gmail.com>
2015-09-16 03:37:34 -0600

    Fix SMTP parameters

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
 		{
 			:subject => "Contact Form",
 			:to => "lgraba@gmail.com",
-			:from => %("#{name}" <#{email})
+			:from => %("#{name}" <#{email}>)
 		}
 	end
 end

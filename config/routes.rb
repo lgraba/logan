5493a518d59e920c2d6bd20f60d59627423dfc6e
Logan Graba <logangraba@gmail.com>
2015-10-02 22:54:04 -0600

    Active links in menu and some links/routing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
   get 'welcome/index'
   root 'welcome#index'
-  get 'resume' => 'resumes#show'
+  get 'resume' => 'resumes#index'
   get 'about' => 'about#index'
   get '*path' => redirect('/')
 end

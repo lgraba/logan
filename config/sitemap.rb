e9d509d28c8418092d8ca835e4d070018997df7c
Logan Graba <logangraba@gmail.com>
2015-10-07 00:27:13 -0600

    Can't get sitemap to S3 bucket

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -28,13 +28,13 @@
 
 # config/sitemap.rb
 
-
+require 'sitemap_generator'
 
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.logangraba.com"
 
 # pick a place safe to write the files
-SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.public_path = 'tmp/sitemaps'
 
 SitemapGenerator::Sitemap.create do
   add '/projects', 'changefreq': 'weekly'
@@ -46,14 +46,16 @@ SitemapGenerator::Sitemap.create do
   add '/about', 'changefreq': 'weekly'
 end
 
-# store on S3 using Fog
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
-
 # inform the map cross-linking where to find the other maps
 SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com/"
 
 # pick a namespace within your bucket to organize your maps
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
+# store on S3 using Fog
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
+
+
+
 # Ping Google and Bing
-SitemapGenerator::Sitemap.ping_search_engines
+# SitemapGenerator::Sitemap.ping_search_engines

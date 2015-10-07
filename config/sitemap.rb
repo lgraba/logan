7b576d7a3879e857dcca70d22f263e79c1565c9b
Logan Graba <logangraba@gmail.com>
2015-10-06 22:51:34 -0600

    Trying to get sitemap generator working on Heroku

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -49,13 +49,13 @@ SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['FOG_DIRECTORY']}.s3.ama
 # pick a namespace within your bucket to organize your maps
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
-  fog_provider: 'AWS',
-  aws_access_key_id: ENV[AWS_ACCESS_KEY_ID],
-  aws_secret_access_key: ENV[AWS_SECRET_ACCESS_KEY],
-  fog_directory: ENV[S3_BUCKET_NAME],
-  fog_region: us-west-1
-)
+# SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
+#   fog_provider: 'AWS',
+#   aws_access_key_id: ENV[AWS_ACCESS_KEY_ID],
+#   aws_secret_access_key: ENV[AWS_SECRET_ACCESS_KEY],
+#   fog_directory: ENV[S3_BUCKET_NAME],
+#   fog_region: us-east-1
+# )
 
 
 SitemapGenerator::Sitemap.ping_search_engines
